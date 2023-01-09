f3d266b9077e9f2bce600f7fe28554e12653a5ac
Remie Malik <remiemalik@gmail.com>
2023-01-09 15:54:51 +0100

    First commit

---
+++ src/main/java/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,21 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDao = context.getBean("accountDAO", AccountDAO.class);
+		
+		theAccountDao.addAccount();
+		
+		context.close();
+	}
+
+}
